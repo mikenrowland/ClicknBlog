1de032d392eaa507eabe4f48b9678891f92e35e5
mykie88 <mykhelmyers@gmail.com>
2021-05-05 00:32:04 +0100

    added allowed hosts

--- env_vars/settings.py
+++ env_vars/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clicknblog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
