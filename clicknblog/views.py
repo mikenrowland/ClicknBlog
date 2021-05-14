17f65cc504226916268c6067a65f1ce62224651d
mykie88 <mykhelmyers@gmail.com>
2021-05-14 01:24:23 +0100

    added a smtp service

--- clicknblog/views.py
+++ clicknblog/views.py
@@ -103,16 +103,16 @@ def password_reset_request(request):
 					email_template_name = "password/password_reset_email.txt"
 					c = {
 					"email":user.email,
-					'domain':'127.0.0.1:8000',
-					'site_name': 'Website',
+					'domain':'clicknblog.herokuapp.com',
+					'site_name': 'clicknblog',
 					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
 					"user": user,
 					'token': default_token_generator.make_token(user),
-					'protocol': 'http',
+					'protocol': 'https',
 					}
 					email = render_to_string(email_template_name, c)
 					try:
-						send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+						send_mail(subject, email, 'mykhelmyers@gmail.com' , [user.email], fail_silently=False)
 					except BadHeaderError:
 						return HttpResponse('Invalid header found.')
 					return redirect ("/password_reset/done/")
--- env_vars/settings.py
+++ env_vars/settings.py
@@ -136,4 +136,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.elasticemail.com'
+EMAIL_PORT = 2525
+EMAIL_HOST_USER = 'mykhelmyers@gmail.com'
+EMAIL_HOST_PASSWORD = '1AE051422E5C70317179A59048E7F1273E22'
+EMAIL_USE_TLS = True
+
+
+DEFAULT_FROM_EMAIL = 'mykhelmyers@gmail.com'
